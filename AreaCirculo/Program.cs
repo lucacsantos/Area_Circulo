ab097070f5d9a001429ed8c1f45457c76dc88ff2
Your Name <you@example.com>
2022-03-16 10:33:20 -0300

    first commit

---
+++ AreaCirculo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do raio: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            AreaCirculo(x);
+        }
+
+        public static double AreaCirculo(double n1)
+        {
+            double a = (n1 * n1) * 3.14;
+
+            Console.WriteLine("Área do circulo " + a.ToString("F2", CultureInfo.InvariantCulture));
+
+            return a;
+
+        }
+    }
+}
